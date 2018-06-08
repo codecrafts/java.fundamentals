8a9021134c6934d786149c9da373899ff8101c2b
unciaal <tkn81@yandex.ru>
2018-06-09 00:08:58 +0300

    Add Konstantin

---
+++ Konstantin/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+	Scanner in = new Scanner (System.in);
+	System.out.print("Enter number 1 = ");
+	double a = in.nextDouble();
+
+	System.out.print("Enter number 2 = ");
+	double b = in.nextDouble();
+
+	System.out.print("Сhoose action ( + - * / %) = ");
+	String с = in.next();
+
+	if (с.equals("+") )
+		System.out.println( a + " + " + b + " = " + (a + b) );
+	else if (с.equals("-") )
+		System.out.println( a + " - " + b + " = " + (a - b) );
+	else if (с.equals("*") )
+		System.out.println( a + " * " + b + " = " + (a * b) );
+	else if (с.equals("/") )
+		System.out.println( a + " / " + b + " = " + (a / b) );
+	else if (с.equals("%") )
+		System.out.println( a + " % " + b + " = " + (a % b) );
+	else
+		System.out.print("invalid action");
+
+    }
+}
