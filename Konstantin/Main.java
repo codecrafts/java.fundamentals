b79308f0b55a4a5f45e6a96fc87cb4512c84203c
unciaal <tkn81@yandex.ru>
2018-06-12 22:46:04 +0300

    loops

--- Konstantin/Main.java
+++ Konstantin/Main.java
@@ -4,29 +4,36 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-	Scanner in = new Scanner (System.in);
-	System.out.print("Enter number 1 = ");
-	double a = in.nextDouble();
 
-	System.out.print("Enter number 2 = ");
-	double b = in.nextDouble();
+        String flag = "u";
+        while(flag.equals("u")) {
+            Scanner in = new Scanner(System.in);
+            System.out.print(" continue enter u, exit enter x = ");
+            flag = in.next();
+            if (flag.equals("x")) break;
 
-	System.out.print("Сhoose action ( + - * / %) = ");
-	String с = in.next();
+            System.out.print("Enter number 1 = ");
+            double a = in.nextDouble();
 
-	if (с.equals("+") )
-		System.out.println( a + " + " + b + " = " + (a + b) );
-	else if (с.equals("-") )
-		System.out.println( a + " - " + b + " = " + (a - b) );
-	else if (с.equals("*") )
-		System.out.println( a + " * " + b + " = " + (a * b) );
-	else if (с.equals("/") )
-		System.out.println( a + " / " + b + " = " + (a / b) );
-	else if (с.equals("%") )
-		System.out.println( a + " % " + b + " = " + (a % b) );
-	else
-		System.out.print("Invalid Action");
-	//Для pull request 
+            System.out.print("Enter number 2 = ");
+            double b = in.nextDouble();
+
+            System.out.print("Сhoose action ( + - * / %) = ");
+            String с = in.next();
+
+            if (с.equals("+"))
+                System.out.println(a + " + " + b + " = " + (a + b));
+            else if (с.equals("-"))
+                System.out.println(a + " - " + b + " = " + (a - b));
+            else if (с.equals("*"))
+                System.out.println(a + " * " + b + " = " + (a * b));
+            else if (с.equals("/"))
+                System.out.println(a + " / " + b + " = " + (a / b));
+            else if (с.equals("%"))
+                System.out.println(a + " % " + b + " = " + (a % b));
+            else
+                System.out.print("Invalid Action");
+        }
 
     }
 }
