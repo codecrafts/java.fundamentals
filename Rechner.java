7b0edcf0a07bd9560f378ff2bd36c46f6bce40af
Dinar Khalfin <dinarhalfin@yahoo.com>
2018-06-13 13:35:50 +0300

    Add files via upload
    add while cycle from Dinar

--- Rechner.java
+++ Rechner.java
@@ -3,26 +3,51 @@ import java.util.Scanner;
 public class Rechner {
 
     public static void main(String[] args) {
+
         Scanner in = new Scanner (System.in);
         System.out.println("Please enter the first number: ");
         int x = in.nextInt();
-        System.out.println("Please enter the first number: ");
+        System.out.println("Please enter the second number: ");
         int y = in.nextInt();
         System.out.println("Now you need to enter the operation: ");
         String z = in.next();
+        int result = 0;
 
         if (z.equals("+"))
-            System.out.println(x + " + " + y + " = " + (x + y));
+            System.out.println(x + " + " + y + " = " + (result = x + y));
         else if (z.equals("/"))
-            System.out.println(x + " / " + y + " = " + (x / y));
+            System.out.println(x + " / " + y + " = " + (result = x / y));
         else if (z.equals("*"))
-            System.out.println(x + " * " + y + " = " + (x * y));
+            System.out.println(x + " * " + y + " = " + (result = x * y));
         else if (z.equals("^"))
-            System.out.println(x + " ^ " + y + " = " + (x ^ y));
+            System.out.println(x + " ^ " + y + " = " + (result = x ^ y));
         else if (z.equals("&"))
-            System.out.println(x + " & " + y + " = " + (x & y));
+            System.out.println(x + " & " + y + " = " + (result = x & y));
         else
             System.out.print("The operation was not identified. Try again.");
+
+        int i = 0;
+        while (i < 10) {
+            System.out.println("Enter the operation again: ");
+            String r = in.next();
+            System.out.println("Please enter the third number: ");
+            int b = in.nextInt();
+            i++;
+
+            if (r.equals("+"))
+                System.out.println(b + " + " + result + " = " + (b + result));
+            else if (r.equals("/"))
+                System.out.println(b + " / " + result + " = " + (b / result));
+            else if (r.equals("*"))
+                System.out.println(b + " * " + result + " = " + (b * result));
+            else if (r.equals("^"))
+                System.out.println(b + " ^ " + result + " = " + (b ^ result));
+            else if (r.equals("&"))
+                System.out.println(b + " & " + result + " = " + (b & result));
+            else
+                System.out.print("The operation was not identified. Try again.");
+        }
+
     }
 }
 
