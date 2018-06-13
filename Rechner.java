d90f6750743dedc5742e0a8615cef72c688500ea
Dinar Khalfin <dinarhalfin@yahoo.com>
2018-06-13 21:42:17 +0300

    Update Rechner.java
    update while

--- Rechner.java
+++ Rechner.java
@@ -2,59 +2,47 @@ package ru.codecrafts;
 import java.util.Scanner;
 public class Rechner {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
         Scanner in = new Scanner(System.in);
-        System.out.println("Please enter the first number: ");
-        int x = in.nextInt();
-        System.out.println("Please enter the second number: ");
-        int y = in.nextInt();
-        System.out.println("Now you need to enter the operation: ");
-        String z = in.next();
+        String button = "y";
         int result = 0;
-
-        if (z.equals("+"))
-            System.out.println(x + " + " + y + " = " + (result = x + y));
-        else if (z.equals("/"))
-            System.out.println(x + " / " + y + " = " + (result = x / y));
-        else if (z.equals("*"))
-            System.out.println(x + " * " + y + " = " + (result = x * y));
-        else if (z.equals("^"))
-            System.out.println(x + " ^ " + y + " = " + (result = x ^ y));
-        else if (z.equals("&"))
-            System.out.println(x + " & " + y + " = " + (result = x & y));
-        else
-            System.out.print("The operation was not identified. Try again.");
-
+        int y;
         int i = 0;
-        String button = "yes";
-        while (i < 1000 & button.equals("yes"))
+        while (button.equals("y"))
         {
-            Scanner big = new Scanner(System.in);
-            System.out.print("To continue press yes, to exit press no: ");
-            button = big.next();
-            if (button.equals("no"))
+            System.out.print("To continue press y, to exit press n: ");
+            button = in.next();
+            if (button.equals("n"))
                 break;
 
-            System.out.println("Enter the operation again: ");
-            String r = in.next();
-            System.out.println("Please enter the third number: ");
-            int b = in.nextInt();
-            i++;
+        System.out.println("Please enter the first number: ");
+        int x = in.nextInt();
+        if (i == 0 )
+        {
+            System.out.println("Please enter the second number: ");
+            y = in.nextInt();
+        }
+        else y = result;
 
-            if (r.equals("+"))
-                System.out.println(b + " + " + result + " = " + (b + result));
-            else if (r.equals("/"))
-                System.out.println(b + " / " + result + " = " + (b / result));
-            else if (r.equals("*"))
-                System.out.println(b + " * " + result + " = " + (b * result));
-            else if (r.equals("^"))
-                System.out.println(b + " ^ " + result + " = " + (b ^ result));
-            else if (r.equals("&"))
-                System.out.println(b + " & " + result + " = " + (b & result));
+        System.out.println("Now you need to enter the operation: ");
+        String z = in.next();
+
+            if (z.equals("+"))
+                System.out.println(x + " + " + y + " = " + (result = x + y));
+            else if (z.equals("/"))
+                System.out.println(x + " / " + y + " = " + (result = x / y));
+            else if (z.equals("*"))
+                System.out.println(x + " * " + y + " = " + (result = x * y));
+            else if (z.equals("^"))
+                System.out.println(x + " ^ " + y + " = " + (result = x ^ y));
+            else if (z.equals("&"))
+                System.out.println(x + " & " + y + " = " + (result = x & y));
             else
                 System.out.print("The operation was not identified. Try again.");
-            }
+            i++;
         }
     }
+}
+
 
