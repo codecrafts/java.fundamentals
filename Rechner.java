bc6ba7d0515e5c2bd7485e2683efc811a72984e5
Dinar Khalfin <dinarhalfin@yahoo.com>
2018-06-14 15:14:45 +0300

    for cycle
    add switch and renamed the variables + for cycle

--- Rechner.java
+++ Rechner.java
@@ -1,46 +1,56 @@
 package ru.codecrafts;
 import java.util.Scanner;
-public class Rechner {
-
+public class Rechner
+{
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        String button = "y";
+        String button = "Y";
         int result = 0;
-        int y;
-        int i = 0;
-        while (button.equals("y"))
+        int zahl2;
+
+        for (;button.equals("go")|button.equals("Y");)
         {
-            System.out.print("To continue press y, to exit press n: ");
+            System.out.println("Please enter the first number: ");
+            int zahl1 = in.nextInt();
+
+            if (button.equals("Y") )
+            {
+                System.out.println("Please enter the second number: ");
+                zahl2 = in.nextInt();
+            }
+            else zahl2 = result;
+
+            System.out.println("Now you need to enter the operation: ");
+            String operation = in.next();
+
+            switch (operation)
+            {
+                case "+":
+                    result = zahl1 + zahl2;
+                    System.out.println(zahl1 + " + " + zahl2 + " = " + result);
+                    break;
+                case "/":
+                    result = zahl1 / zahl2;
+                    System.out.println(zahl1 + " / " + zahl2 + " = " + result);
+                    break;
+                case "*":
+                    result = zahl1 * zahl2;
+                    System.out.println(zahl1 + " * " + zahl2 + " = " + result);
+                    break;
+                case "^":
+                    result = zahl1 ^ zahl2;
+                    System.out.println(zahl1 + " ^ " + zahl2 + " = " + result);
+                    break;
+                case "&":
+                    result = zahl1 & zahl2;
+                    System.out.println(zahl2 + " & " + zahl2 + " = " + result);
+                    break;
+                default:
+                    System.out.print("The operation was not identified. Try again.");
+            }
+            System.out.println("To continue press go, to exit press exit");
             button = in.next();
-            if (button.equals("n"))
-                break;
-
-        System.out.println("Please enter the first number: ");
-        int x = in.nextInt();
-        if (i == 0 )
-        {
-            System.out.println("Please enter the second number: ");
-            y = in.nextInt();
-        }
-        else y = result;
-
-        System.out.println("Now you need to enter the operation: ");
-        String z = in.next();
-
-            if (z.equals("+"))
-                System.out.println(x + " + " + y + " = " + (result = x + y));
-            else if (z.equals("/"))
-                System.out.println(x + " / " + y + " = " + (result = x / y));
-            else if (z.equals("*"))
-                System.out.println(x + " * " + y + " = " + (result = x * y));
-            else if (z.equals("^"))
-                System.out.println(x + " ^ " + y + " = " + (result = x ^ y));
-            else if (z.equals("&"))
-                System.out.println(x + " & " + y + " = " + (result = x & y));
-            else
-                System.out.print("The operation was not identified. Try again.");
-            i++;
         }
     }
 }
