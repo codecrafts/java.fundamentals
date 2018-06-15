11ae04ef5df10cb60f02b37ca1e854d42cbeaa86
Dinar Khalfin <dinarhalfin@yahoo.com>
2018-06-15 23:12:09 +0300

    добавил файл
    add Rechner

--- Dinar/Rechner.java
+++ Dinar/Rechner.java
@@ -1,28 +1,55 @@
 package ru.codecrafts;
+import java.util.ArrayList;
 import java.util.Scanner;
-public class Rechner {
+public class Rechner
+{
+    public static void main(String[] args)
+    {
+            ArrayList<Integer> digits = new ArrayList<>();
+            Scanner big = new Scanner(System.in);
+            String button = "yes";
+            int result;
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner (System.in);
-        System.out.println("Please enter the first number: ");
-        int x = in.nextInt();
-        System.out.println("Please enter the first number: ");
-        int y = in.nextInt();
-        System.out.println("Now you need to enter the operation: ");
-        String z = in.next();
+            System.out.println("Please enter so much numbers as you wish. You have only 5 operations to choose: +, /, *, ^, &  ");
 
-        if (z.equals("+"))
-            System.out.println(x + " + " + y + " = " + (x + y));
-        else if (z.equals("/"))
-            System.out.println(x + " / " + y + " = " + (x / y));
-        else if (z.equals("*"))
-            System.out.println(x + " * " + y + " = " + (x * y));
-        else if (z.equals("^"))
-            System.out.println(x + " ^ " + y + " = " + (x ^ y));
-        else if (z.equals("&"))
-            System.out.println(x + " & " + y + " = " + (x & y));
-        else
-            System.out.print("The operation was not identified. Try again.");
+            for(;button.equals("yes");)
+            {
+                if (big.hasNextInt())
+                    digits.add(big.nextInt());
+                else
+                    button = "no";
+            }
+
+            String operation = big.next() ;
+            result = digits.get(0);
+
+            for ( int i = 1;i<digits.size();i++)
+            {
+                switch (operation)
+                    {
+                        case "+":
+                            result = result + digits.get(i);
+                            break;
+                        case "/":
+                            result = result / digits.get(i);
+                            break;
+                        case "*":
+                            result = result * digits.get(i);
+                            break;
+                        case "^":
+                            result = result ^ digits.get(i);
+                            break;
+                        case "&":
+                            result = result & digits.get(i);
+                            break;
+                        default:
+                            System.out.print("You have made wrong action.");
+                    }
+            }
+        System.out.println(result);
     }
 }
+
+
+
 
