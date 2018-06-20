a16ae7e013508d9d9364315c47e5da1cfc24f7b0
unciaal <tkn81@yandex.ru>
2018-06-20 22:50:57 +0300

    Использовал новый подход

--- Konstantin/Kalknew.java
+++ Konstantin/Kalknew.java
@@ -2,23 +2,26 @@ import java.util.Scanner;
 
 public class Kalknew {
 
-        static double number1;
-        static double number2;
-        static String arithmeticOperation;
         public static void main(String[] args) {
             double res;
-            inputData();
+            double number1 = inputNumber("Введите первое число");
+            double number2 = inputNumber("Введите второе число");
+            String arithmeticOperation = operation("Какое действие надо произвести над числами?");
             res = result(number1, number2, arithmeticOperation);
             System.out.print(" " + number1 + " " + arithmeticOperation + " " + number2 + " = " + res );
         }
-        static void inputData () {
+        static double inputNumber (String text){
+            double number;
             Scanner in = new Scanner(System.in);
-            System.out.println("Введите первое число");
-            number1 = in.nextDouble();
-            System.out.println("Введите второе число");
-            number2 = in.nextDouble();
-            System.out.println("Какое действие надо произвести над числами?");
-            arithmeticOperation = in.next();
+            System.out.println(text);
+            number = in.nextDouble();
+            return number;
+        }
+        static String operation (String text){
+            String operation;
+            Scanner in = new Scanner(System.in);
+            operation = in.next();
+            return operation;
         }
         static double result (double num1, double num2, String oper){
             double result = 0;
