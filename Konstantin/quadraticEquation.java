10e1d0a77819b46e71ff60fb4515df066acef534
unciaal <tkn81@yandex.ru>
2018-06-22 22:56:16 +0300

    Поправил добавил тесты

--- Konstantin/quadraticEquation.java
+++ Konstantin/quadraticEquation.java
@@ -6,30 +6,31 @@ public class quadraticEquation {
     // вида a*x*x + bx + c = 0; (a,b,c - задается пользователем)
     public static void main(String[] args) {
         double a = coefficient("Введите старший коэффициент (а)");
+
         while (a < 0) {
             System.out.println("Старший коэффициент не может быть меньше 0");
             a = coefficient("Введите старший коэффициент (а)");
         }
+        
         double b = coefficient("Введите коэффициент при х (b)");
         double c = coefficient("Введите свободный член (c)");
 
-        double d = discriminantSearch(a,b,c);
+        double d = discriminantSearch(a, b, c);
         double result;
         if (d < 0) {
             System.out.println(d + " < 0 - квадратное уравнение не имеет корней");
-        }
-        else if (d == 0) {
+        } else if (d == 0) {
             System.out.println(d + " = 0 - квадратное уравнение имеет 1 корнь");
             result = solutionOfEquation(a, b, d);
             System.out.println("корень уравнения равен x = " + result);
-        }
-        else if (d > 0) {
+        } else if (d > 0) {
             System.out.println(d + " > 0 - квадратное уравнение имеет 2 корня");
             result = solutionOfEquation(a, b, Math.sqrt(d));
             System.out.println("1 корень уравнения равен x1 = " + result);
             result = solutionOfEquation(a, b,- Math.sqrt(d));
             System.out.println("2 корень уравнения равен x2 = " + result);
         }
+        test();
     }
     static double coefficient (String text) {
         double value;
@@ -47,6 +48,21 @@ public class quadraticEquation {
         double result = (-b + rootOfNumber) / 2 * a;
         return result;
     }
+    static void test() {
+        double discriminantTest = discriminantSearch(1,2,1);
+        if (discriminantTest == 0) {
+            System.out.println("Проверка дискриминанта пройдена");
+        } else System.out.println("В методе по поиску дискрименанта ошибка");
+
+        double solutionTest = solutionOfEquation(1,1,3);
+        if ( solutionTest == 1) {
+            System.out.println("Проверка решения пройдена");
+        } else System.out.println("В методе решения есть ошибка");
+
+    }
+
+
+
 
 
 }
