fcb55caa06835eeb6dc42400b4de646503687cbc
Dinar Khalfin <dinarhalfin@yahoo.com>
2018-06-23 15:09:42 +0300

    Methods
    Used simple Methods

--- Dinar1/Rechner.java
+++ Dinar1/Rechner.java
@@ -1,56 +1,47 @@
 package ru.codecrafts;
-import java.util.ArrayList;
 import java.util.Scanner;
-public class Rechner
-{
-    public static void main(String[] args)
-    {
-            ArrayList<Integer> digits = new ArrayList<>();
-            Scanner big = new Scanner(System.in);
-            String button = "yes";
-            int result = 0;
 
-            System.out.println("Please, enter so many numbers as you wish. You have only 5 operations to choose from these: +, /, *, ^, &  ");
+public class Rechner {
+    static int digit1;
+    static int digit2;
+    static String operating;
+    static Integer outcome;
 
-            for(;button.equals("yes");)
-            {
-                if (big.hasNextInt())
-                    digits.add(big.nextInt());
-                else
-                    button = "no";
-            }
+    public static void main(String[] args) {
+        enterMetrics();
+        outcome();
+        System.out.print(digit1 + " " + operating + " " + digit2 + " = " + outcome);
+    }
 
-            String operation = big.next() ;
-            if (digits.size() > 0) 
-                result = digits.get(0);
-            
-            for (int i = 1;i < digits.size();i++)
-            {
-                switch (operation)
-                    {
-                        case "+":
-                            result = result + digits.get(i);
-                            break;
-                        case "/":
-                            result = result / digits.get(i);
-                            break;
-                        case "*":
-                            result = result * digits.get(i);
-                            break;
-                        case "^":
-                            result = result ^ digits.get(i);
-                            break;
-                        case "&":
-                            result = result & digits.get(i);
-                            break;
-                        default:
-                            System.out.print("You have made wrong action.");
-                    }
-            }
-        System.out.println(result);
+    static void enterMetrics() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please, enter the first number: ");
+        digit1 = in.nextInt();
+        System.out.println("Please, enter the second number: ");
+        digit2 = in.nextInt();
+        System.out.println("Now you need to enter the operation: ");
+        operating = in.next();
+    }
+
+    static void outcome(){
+        switch (operating){
+            case "+":
+                outcome = digit1 + digit2;
+                break;
+            case "/":
+                outcome = digit1 / digit2;
+                break;
+            case "*":
+                outcome = digit1 * digit2;
+                break;
+            case "^":
+                outcome = digit1 ^ digit2;
+                break;
+            case "&":
+                outcome = digit1 & digit2;
+                break;
+            default:
+                System.out.println("The operation was not identified. Try again, please!");
+        }
     }
 }
-
-
-
-
