6123c4b70e4bac3a1338a65a8b992eed1d3c67ee
Zaqwesx <aes_sedai@mail.ru>
2018-07-07 00:14:17 +0300

    Lesson1

--- Stas/src/Lesson1.java
+++ Stas/src/Lesson1.java
@@ -2,13 +2,26 @@ public class Lesson1 {
     public static void main(String[] args){
 
         //Определяем переменные.
-        float E, g = 9.8f, m = 10.3f, h = 12.6f, V = 33.2f;
+        double Energy;
+        double gravity = 9.8; // Явное объявление ускорения свободного падения.
+        double mass = 10.3; //Явное объявление массы тела.
+        double heigth = 12.6; // Явное объявление высоты тела над поверхностью Земли.
+        double Velocity = 33.2; // Явное объявление скорости тела.
 
         // Рассчитываем значение энергии по формуле.
-        E = m*g*h + m*V*V/2;
+        Energy = mass*gravity*heigth + mass*Velocity*Velocity/2;
 
         //Выводим результат в консоль.
-        System.out.print("Результат = " + E);
+        System.out.println();
+        System.out.println("При заданных параметрах рассчета:");
+        System.out.println("Масса тела = " + mass +" кг.");
+        System.out.println("Высота тела над поверхностью Земли = " + heigth +" м.");
+        System.out.println("Ускорение свободного падения в атмосфере Земли = " + gravity +" м/с^2.");
+        System.out.println("Скорость движения тела = " + Velocity +" м/с.");
+        System.out.println();
+        System.out.print("Полная энергия тела = ");
+        System.out.printf("%.2f",Energy);
+        System.out.println(" Дж.");
 
 
     }
