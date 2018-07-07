fda02543e5a3b70fd0c3a650609c25f55ccfab68
Zaqwesx <aes_sedai@mail.ru>
2018-07-07 13:43:42 +0300

    variables to lower case

--- Stas/src/Lesson1.java
+++ Stas/src/Lesson1.java
@@ -2,14 +2,14 @@ public class Lesson1 {
     public static void main(String[] args){
 
         //Определяем переменные.
-        double Energy;
+        double energy;
         double gravity = 9.8; // Явное объявление ускорения свободного падения.
         double mass = 10.3; //Явное объявление массы тела.
         double heigth = 12.6; // Явное объявление высоты тела над поверхностью Земли.
-        double Velocity = 33.2; // Явное объявление скорости тела.
+        double velocity = 33.2; // Явное объявление скорости тела.
 
         // Рассчитываем значение энергии по формуле.
-        Energy = mass*gravity*heigth + mass*Velocity*Velocity/2;
+        energy = mass*gravity*heigth + mass*velocity*velocity/2;
 
         //Выводим результат в консоль.
         System.out.println();
@@ -17,10 +17,10 @@ public class Lesson1 {
         System.out.println("Масса тела = " + mass +" кг.");
         System.out.println("Высота тела над поверхностью Земли = " + heigth +" м.");
         System.out.println("Ускорение свободного падения в атмосфере Земли = " + gravity +" м/с^2.");
-        System.out.println("Скорость движения тела = " + Velocity +" м/с.");
+        System.out.println("Скорость движения тела = " + velocity +" м/с.");
         System.out.println();
         System.out.print("Полная энергия тела = ");
-        System.out.printf("%.2f",Energy);
+        System.out.printf("%.2f",energy);
         System.out.println(" Дж.");
 
 
