809d5db5db71b4e15f2c5f1b887203b6e322691e
Zaqwesx <aes_sedai@mail.ru>
2018-07-12 02:00:05 +0300

    fixed style and input doublecheck

--- Stas/src/Lesson3.java
+++ Stas/src/Lesson3.java
@@ -14,14 +14,11 @@ public class Lesson3 {
 
         System.out.println("Введите первый операнд:");  // Запрос на ввод первого операнда.
 
-        while(input.hasNext())      // Метод hasNext() останавливает выполнение программы
-        {                           // и ожидает от пользователя ввода в консоль. В случае ввода input.hasNext() = true.
-            if(input.hasNextDouble()) // Проверяем, является ли введенный текст элементом типа double.
-            {
-                operand1 = input.nextDouble();  // Если ввели double, то значению первого операнда присваиваем то,
-                break;                          // что ввел пользователь.
-            }
-            else{                               // Если введен не double, то сообщаем о некорректном вводе и просим повторить.
+        while(input.hasNext()){      // Метод hasNext() останавливает выполнение программы и ожидает от пользователя ввода в консоль.
+            if(input.hasNextDouble()){ // В случае ввода input.hasNext() = true. Проверяем, является ли введенный текст элементом типа double.
+                operand1 = input.nextDouble();  // Если ввели double, то значению первого операнда присваиваем то, что ввел пользователь.
+                break;
+            }else{                               // Если введен не double, то сообщаем о некорректном вводе и просим повторить.
                 System.out.println("Введенное значение не является числом. Повторите ввод:");
                 input.next();       // Очищаем значение в input и переходим к считыванию нового.
             }
@@ -29,14 +26,11 @@ public class Lesson3 {
 
         System.out.println("Введите второй операнд:");
 
-        while(input.hasNext())
-        {
-            if(input.hasNextDouble())
-            {
+        while(input.hasNext()){
+            if(input.hasNextDouble()){
                 operand2 = input.nextDouble();
                 break;
-            }
-            else{
+            }else{
                 System.out.println("Введенное значение не является числом. Повторите ввод:");
                 input.next();
             }
@@ -44,53 +38,29 @@ public class Lesson3 {
 
         System.out.println("Введите знак операции:");
 
-        while(input.hasNext())
-        {
+        while(input.hasNext()){
             operator = input.next().trim(); // Считываем знак операции. Делаем trim на случай лишних пробелов.
-            if ( operator.equals("+")) {    // Проверяем, корректно ли введен знак.
-                break;
-            }
-            if ( operator.equals("-")) {
-                break;
-            }
-            if ( operator.equals("%")) {
-                break;
-            }
-            if ( operator.equals("/")) {
-                break;
-            }
-            if ( operator.equals("*")) {
-                break;
-            }
-            else{
-                System.out.println("Знак операции задан неверно. Повторите ввод:");
-            }
-        }
-
-        switch (operator) {                     // Рассчитываем result в зависимости от введенного знака.
-            case "+": {
+            if (operator.equals("+")){    // Рассчитываем result в зависимости от введенного знака.
                 result = operand1 + operand2;
                 break;
             }
-            case "-": {
+            if (operator.equals("-")){
                 result = operand1 - operand2;
                 break;
             }
-            case "/": {
-                result = operand1 / operand2;
-                break;
-            }
-            case "%": {
+            if (operator.equals("%")){
                 result = operand1 % operand2;
                 break;
             }
-            case "*": {
-                result = operand1 * operand2;
+            if (operator.equals("/")){
+                result = operand1 / operand2;
                 break;
             }
-            default: {                  // В случае некорректного ввода, отправляем пользователя на повторный ввод.
-                System.out.println("Неверный знак операции. Повторите ввод:");
-                input.next();
+            if (operator.equals("*")){
+                result = operand1 * operand2;
+                break;
+            }else{
+                System.out.println("Знак операции задан неверно. Повторите ввод:");
             }
         }
 
