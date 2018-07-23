26a01a9e2502bc94d0c1aae03375424f56097ad9
Zaqwesx <aes_sedai@mail.ru>
2018-07-23 21:45:47 +0300

    lesson5. post-review #71

--- Tracks/Zaqwesx/lesson5/Lesson5.java
+++ Tracks/Zaqwesx/lesson5/Lesson5.java
@@ -7,20 +7,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lesson5 {
+    public static final int BYTE_IN_INT = 8; // Константа. Количество byte в int.
+
     public static void main(String[] args) {
-
         int decNumber = inputNat();
-        String hex = hexNums(reverseOfArray(decToHex(inputNat())));
-        int subIndex = 0;
-
-        for (int i = 0; i < hex.length(); i++) {   // Считаем сколько лишних нулей в начале числа в ответе.
-            if (hex.charAt(i) != '0') {
-                subIndex = i;
-                break;
-            }
-        }
+        StringBuffer hexNumber = decToHex(decNumber);
         System.out.println("В шестнадцатеричной системе счисления натуральное число "
-                + decNumber + " будет иметь вид: " + hex.substring(subIndex,hex.length()));
+                + decNumber + " будет иметь вид: " + hexNumber);
     }
 
     static int inputNat() {             // Метод проверки ввода числа.
@@ -43,57 +36,47 @@ public class Lesson5 {
             }
         }
 
+        input.close();
         return number;
     }
 
-    static ArrayList<Byte> decToHex(int natNum) {   // Метод перевода из десятичной системы в шестнадцатеричную.
-        ArrayList<Byte> hexMassive = new ArrayList<>();
+    static StringBuffer decToHex(int natNum) {   // Метод перевода из десятичной системы в шестнадцатеричную.
         byte byteNum = 0;
+        StringBuffer hexNum = new StringBuffer("");
 
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < BYTE_IN_INT; i++) {
             byteNum = (byte) (natNum & 0x0F);
-            hexMassive.add(byteNum);
+            hexNum.insert(0,hexNums(Byte.toString(byteNum)));
             natNum >>= 4;
         }
-
-        return hexMassive;
+        return hexNum;
     }
 
-    static ArrayList<Byte> reverseOfArray(ArrayList<Byte> arrayToReverse) {       // Метод для инверсии списка.
-        ArrayList<Byte> reversedArray = new ArrayList<>();
-        for (int i = arrayToReverse.size()-1; i >= 0; i--) {
-            reversedArray.add(arrayToReverse.get(i));
-        }
-        return reversedArray;
-    }
+    static String hexNums(String input) {       // Замена чисел десятичного алфавита на шентснадцатеричный.
+        String numHex = "";
 
-    static String hexNums(ArrayList<Byte> input) {
-        String numHex = "";                                             // находится в начале массива.
-
-        for (Byte i : input) {           // Сопоставление цифр буквам 16-ричного алфавта.
-            switch (Integer.toString(i)) {
-                case "10":
-                    numHex += "A";
-                    break;
-                case "11":
-                    numHex += "B";
-                    break;
-                case "12":
-                    numHex += "C";
-                    break;
-                case "13":
-                    numHex += "D";
-                    break;
-                case "14":
-                    numHex += "E";
-                    break;
-                case "15":
-                    numHex += "F";
-                    break;
-                default:
-                    numHex += i;
-                    break;
-            }
+        switch (input) {
+            case "10":
+                numHex = "A";
+                break;
+            case "11":
+                numHex = "B";
+                break;
+            case "12":
+                numHex = "C";
+                break;
+            case "13":
+                numHex = "D";
+                break;
+            case "14":
+                numHex = "E";
+                break;
+            case "15":
+                numHex = "F";
+                break;
+            default:
+                numHex = input;
+                break;
         }
 
         return numHex;
