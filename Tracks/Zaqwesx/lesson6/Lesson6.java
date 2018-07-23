e86ddee40edf198b40eda0c1f2d486f66d329054
Zaqwesx <aes_sedai@mail.ru>
2018-07-23 19:37:12 +0300

    removed string doublecheck

--- Tracks/Zaqwesx/lesson6/Lesson6.java
+++ Tracks/Zaqwesx/lesson6/Lesson6.java
@@ -11,45 +11,37 @@ public class Lesson6 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Введите IPv4 адрес :");
-        String ip = "";
+        String ip = input.next();
+        ArrayList<String> splitInput = new ArrayList<>();
 
-        while (input.hasNext()) {
-            ip = input.next();
-            if (inputChecker(ip)) {                                     // Проверяем, что вводятся только цифры или точки.
-                ArrayList<String> splitArray = inputSplitter(ip);       // Разбиваем строку на кусочки по точкам.
-
-                if (numChecker(splitArray) && splitArray.size() == 4)   // Проверяем, что кусочки это цифры в диапазоне 0-255
-                    break;                                              // и кусочков всего 4.
-                else
-                    System.out.println("Некорректный ввод. Повторите попытку.");
-            }else
-                System.out.println("Некорректный ввод. Повторите попытку.");
+        for (String element : ip.split("\\.")) {
+            if (inputChecker(element))
+                splitInput.add(element);
+            else
+                break;
         }
 
+        if (numChecker(splitInput) && splitInput.size() == 4)           // Проверяем, что ввод удовлетворяет маске IPv4.
+            System.out.println("Введен корректный IPv4 адрес: " + ip);
+        else
+            System.out.println("Введен некорректный IPv4 адрес.");
+
         input.close();
-        System.out.println("Введен корректный IPv4 адрес: " + ip);
     }
 
     static boolean inputChecker(String input) {
         boolean checker = true;
+
         for (int i = 0; i < input.length(); i++) {
-            if (Character.isDigit(input.charAt(i)) || input.charAt(i) == '.')
+            if (Character.isDigit(input.charAt(i)))
                 continue;
             else {
                 checker = false;
                 break;
             }
         }
+
         return checker;
-    }
-
-    static ArrayList<String> inputSplitter(String input) {
-        ArrayList<String> splitInput = new ArrayList<>();
-
-        for (String element : input.split("\\.")) {
-            splitInput.add(element);
-        }
-        return splitInput;
     }
 
     static boolean numChecker(ArrayList<String> input) {
@@ -65,8 +57,5 @@ public class Lesson6 {
         }
 
         return checker;
-
     }
-
-
 }
