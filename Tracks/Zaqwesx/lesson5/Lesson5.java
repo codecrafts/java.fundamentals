033da78a1a840e76ace267eaaa68724b64a83d2e
Zaqwesx <aes_sedai@mail.ru>
2018-07-25 14:43:00 +0300

    убрал лишние стринги и свич. переделал под массив

--- Tracks/Zaqwesx/lesson5/Lesson5.java
+++ Tracks/Zaqwesx/lesson5/Lesson5.java
@@ -7,13 +7,16 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lesson5 {
-    public static final int BYTE_IN_INT = 8; // Константа. Количество byte в int.
+    public static final int shiftNum = 8;   // В int 32 бита. Каждые 4 бита это коэффициент степени 16. Для получения
+                                            // всех степеней 16 из int необходимо 8 сдвигов.
+    public static final char[] hexSymbols = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
     public static void main(String[] args) {
         int decNumber = inputNat();
-        StringBuffer hexNumber = decToHex(decNumber);
+        String hexValue = new String(decToHex(decNumber));
+
         System.out.println("В шестнадцатеричной системе счисления натуральное число "
-                + decNumber + " будет иметь вид: " + hexNumber);
+                + decNumber + " будет иметь вид: " + hexValue);
     }
 
     static int inputNat() {             // Метод проверки ввода числа.
@@ -28,10 +31,10 @@ public class Lesson5 {
                 if (number > 0) {
                     break;
                 }else {
-                    System.out.println("Введенное значение не является натуральным числом. Повторите ввод:");
+                    System.out.println("Введенно некорректное число. Повторите ввод:");
                 }
             }else {
-                System.out.println("Введенное значение не является натуральным числом. Повторите ввод:");
+                System.out.println("Введенно некорректное число. Повторите ввод:");
                 input.next();
             }
         }
@@ -40,45 +43,15 @@ public class Lesson5 {
         return number;
     }
 
-    static StringBuffer decToHex(int natNum) {   // Метод перевода из десятичной системы в шестнадцатеричную.
-        byte byteNum = 0;
-        StringBuffer hexNum = new StringBuffer();
+    static char[] decToHex(int natNum) {   // Метод перевода из десятичной системы в шестнадцатеричную.
+        byte byteNum;
+        char[] hexNum = new char[8];
 
-        for (int i = 0; i < BYTE_IN_INT; i++) {
+        for (int i = 0; i < shiftNum; i++) {
             byteNum = (byte) (natNum & 0x0F);
-            hexNum.insert(0,Character.toString((char) byteNum));
+            hexNum[hexNum.length - 1 - i] = hexSymbols[byteNum];
             natNum >>= 4;
         }
         return hexNum;
     }
-
-    static String hexNums(String input) {       // Замена чисел десятичного алфавита на шентснадцатеричный.
-        String numHex = "";
-
-        switch (input) {
-            case 1:
-                numHex = "A";
-                break;
-//            case 11:
-////                numHex = "A";
-////                break;
-////            case 12:
-////                numHex = "A";
-////                break; 'A';
-////            case 13:
-////                numHex = "A";
-////                break;
-////            case 14:
-////                numHex = "A";
-////                break;
-////            case 15:
-////                numHex = "A";
-////                break;
-//            default:
-////                numHex = Byte.toString(input);
-//                break;
-//        }
-//
-//        return input;
-//    }
 }
