0d62414222721fa1bfd1ec7edb0684b1894c8d3e
unciaal <tkn81@yandex.ru>
2018-07-27 13:42:27 +0300

    Площадь треугольника
    Переработана по задаче.

---
+++ Konstantin/AreaTriangle/Figure.java
@@ -0,0 +1,6 @@
+//Вычислить площадь (Возвращает NaN, если вычисляется площадь для несуществующей фигуры
+//Проверить валидность фигуры (false - если фигура не валидна)
+public abstract class Figure {
+    abstract double findArea();
+    abstract boolean validationTriangular();
+}
--- Konstantin/AreaTriangle/Tests.java
+++
@@ -1,8 +0,0 @@
-public class Tests {
-    static void checkLogic() {
-        if (Area.findeArea(1,2,3) == 0) System.out.println("(123)тест пройден");
-        if (Area.findeArea(2,2,2) == 1.7320508075688772) System.out.println("(222)тест пройден");
-        if (Area.findeArea(0,0,0) == 0) System.out.println("(000)тест пройден");
-    }
-
-}
