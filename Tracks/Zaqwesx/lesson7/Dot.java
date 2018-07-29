79c94087b13d55dd4a8ebc47ad65aa70d8a09695
Zaqwesx <aes_sedai@mail.ru>
2018-07-29 23:12:44 +0300

    Класс точка

---
+++ Tracks/Zaqwesx/lesson7/Dot.java
@@ -0,0 +1,17 @@
+public class Dot {
+        private Integer x;
+        private Integer y;
+
+    public Integer getX() {
+        return x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public Dot(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+}
