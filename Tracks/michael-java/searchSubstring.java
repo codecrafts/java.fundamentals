2ecc07df0d2f4597c09a25962487d5ffd2784098
Михаил <40785828+michael-java@users.noreply.github.com>
2018-07-30 14:21:22 +0500

    searchSubstring

---
+++ Tracks/michael-java/searchSubstring.java
@@ -0,0 +1,81 @@
+package searchSubstring;
+
+public class searchSubstring {
+
+    static String words = "... мама -папа, (дед+баба) ели к_а_ш_у!";
+    static String separators = "., !-:+";
+
+    public static void main(String[] args) {
+
+        int countWords = 0;
+
+        countWords = countWords ( words, separators );
+        printResult(countWords);
+
+        System.out.println (testCode() ? "тест пройден" : "тест не пройден");
+    }
+
+    static int countWords( String testWords, String testSeparators ){
+
+        boolean isWasLetter = false;   // состояние - буква или нет
+        int counterWords = 0;          // счетчик слов
+
+        for (int index = 0; index < testWords.length(); index++){       // перебор элементов тестируемой строки на буква/не буква
+
+            if ( isLetter( index, testWords, testSeparators ) ){        // элемент строки - буква
+                isWasLetter = true;                                     // меняем состояние на "буква"
+                if( index == testWords.length()-1 ) counterWords++;     // в строке - последняя буква
+            }
+            else{                                                     // элемент строки - не буква
+                if(isWasLetter) counterWords++;                       // сечас не буква, а перед ней - буква, плюсуем слово
+                isWasLetter = false;                                  // меняем состояние на "не буква"
+            }
+        }
+        return counterWords;
+    }
+
+    static boolean isLetter( int index, String whatTest, String withTest ) {    // проверка вхождения подстроки в строку
+
+        char letter = whatTest.charAt ( index );
+        boolean isLetter = true;                                                // состояние элемента тестируемой строки: буква/не буква
+
+        for (int j = 0; j < withTest.length (); j++) {                // проверка элемента тестируемой строки
+            if (letter == withTest.charAt ( j )){                     // поймали разделитель
+                isLetter = false;
+                break;
+            }
+        }
+        return isLetter;
+    }
+
+    public static void printResult(int result){
+
+        System.out.println ("В строке: " + "\"" + words + "\" " + result + " слов");
+    }
+
+    public static boolean testCode () {
+
+        int counter = 0;
+        Boolean testPassed = false;
+
+        if (countWords ("Мамамылараму", " !") == 1)
+            counter++;
+        if (countWords( "       Мамамылараму", " !") == 1)
+            counter++;
+        if (countWords("кто-нибудь - это не кто-то", " -") == 6)
+            counter++;
+        if (countWords("Мамамылараму     ", " ,") == 1)
+            counter++;
+        if (countWords(" Мама мыла раму ", " ") == 3)
+            counter++;
+        if (countWords(" ", " !.") == 0)
+            counter++;
+        if (countWords("", " ") == 0)
+            counter++;
+
+        if (counter == 7)
+            testPassed = true;
+        return testPassed;
+    }
+
+}
