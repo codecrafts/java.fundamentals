13f582d7820280403da018872e3ba24cd0dc759f
Михаил Кобиняков <40785828+michael-java@users.noreply.github.com>
2018-08-15 00:25:36 +0500

    Update

--- Tracks/michael-java/EventI.java
+++ Tracks/michael-java/EventI.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 
 public interface EventI {
 
+    String meet = "Встреча";
+    String task = "Задача";
+
     boolean getEventPreform();
 
     void setEventPerform(boolean perform);
