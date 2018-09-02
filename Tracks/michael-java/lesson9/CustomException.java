044b790c43780b98ff0a53fccddeb3bb45b20eed
Михаил Кобиняков <40785828+michael-java@users.noreply.github.com>
2018-09-02 22:07:45 +0500

    Delete CustomException.java

--- Tracks/michael-java/lesson9/CustomException.java
+++
@@ -1,9 +0,0 @@
-package lesson9;
-
-public class CustomException extends Exception {
-        public CustomException() {}
-
-        public CustomException(String message) {
-            super(message);
-        }
-}
