2f1a1924986c5d0ba02fd8c27d8fdb6ea292d973
Dinar Khalfin <dinarhalfin@yahoo.com>
2018-09-14 14:42:45 +0300

    Update of codestyle in Point
    Updated names of Variables in the class.

--- Tracks/dhalfin/Point.java
+++ Tracks/dhalfin/Point.java
@@ -1,19 +1,19 @@
 package ru.codecrafts;
 
 public class Point {
-    private int x;
-    private int y;
+    private final int X;
+    private final int Y;
 
     public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+        this.X = x;
+        this.Y = y;
     }
 
     public int getX() {
-        return this.x;
+        return this.X;
     }
 
     public int getY() {
-        return this.y;
+        return this.Y;
     }
 }
